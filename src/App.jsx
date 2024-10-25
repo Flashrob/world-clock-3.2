bde93c6ed8b5321c757050b8822f65fd7526489e
wenxin-p <pohwenxin@rocketmail.com>
2024-10-25 22:44:47 +0800

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,76 @@
 import logo from "/logo.png";
 import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import Clock from "./Clock";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
+    <div>
+      <img src={logo} className="logo" alt="Rocket logo" />
       <h1>World Clock</h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {/* This feels violative of the DIY principle, but idk how to make it into a component */}
+        <Container>
+          <Row className="table-row">
+            <Col className="table-col">City</Col>
+            <Col className="table-time">Clock</Col>
+          </Row>
+          <Row className="table-row">
+            <Col className="table-col">
+              <p>Asia/Seoul</p>
+            </Col>
+            <Col className="table-time">
+              <p>
+                <Clock timeZone="Asia/Seoul" />
+              </p>
+            </Col>
+          </Row>
+          <Row className="table-row">
+            <Col className="table-col">
+              <p>Asia/Singapore</p>
+            </Col>
+            <Col className="table-time">
+              <p>
+                <Clock timeZone="Asia/Singapore" />
+              </p>
+            </Col>
+          </Row>
+          <Row className="table-row">
+            <Col className="table-col">
+              <p>Europe/London</p>
+            </Col>
+            <Col className="table-time">
+              <p>
+                <Clock timeZone="Europe/London" />
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App;
+// This works too but the boxes are not aligned
+// <div className="card">
+//     <Container>
+//       <Row>
+//         <Col>City</Col>
+//         <Col>Clock</Col>
+//         </Row>
+//           <Clock timeZone="Asia/Seoul" />
+//         </Row>
+//         <Row>
+//           <Clock timeZone="Asia/Singapore" />
+//         </Row>
+//         <Row>
+//           <Clock timeZone="Europe/London" />
+//         </Row>
+//       </Row>
+//     </Container>
+//   </div>
+// </div>
